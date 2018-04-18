bac102287d1c4653b83ac9c88c573b503ce8bd60
tom.zhang <tom.zhang@all-dream.com>
2018-04-18 16:47:30 +0800

    update

---
+++ banana-platform-config/src/main/java/com/burgess/banana/App.java
@@ -0,0 +1,13 @@
+package com.burgess.banana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
