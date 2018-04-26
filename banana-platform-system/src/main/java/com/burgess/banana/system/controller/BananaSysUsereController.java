bab467f4a54fa38b64045158e7a7027797e8b942
burgess <burgess_zhang@163.com>
2018-04-26 15:49:53 +0800

    update

---
+++ banana-platform-system/src/main/java/com/burgess/banana/system/controller/BananaSysUsereController.java
@@ -0,0 +1,18 @@
+package com.burgess.banana.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @project banana-platform-system
+ * @package com.burgess.banana.system.controller
+ * @file BananaSysUsereController.java
+ * @author burgess.zhang
+ * @time 下午3:08:33 2018年4月26日
+ * @desc user Controller
+ */
+@RestController
+@RequestMapping("/sys/user")
+public class BananaSysUsereController {
+
+}
