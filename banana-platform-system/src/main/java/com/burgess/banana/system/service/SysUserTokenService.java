c9e4d34a012f6053d192d9155f39c852cb23079f
tom.zhang <tom.zhang@all-dream.com>
2018-05-23 15:32:53 +0800

    Update

---
+++ banana-platform-system/src/main/java/com/burgess/banana/system/service/SysUserTokenService.java
@@ -0,0 +1,28 @@
+package com.burgess.banana.system.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.burgess.banana.system.entity.SysUserTokenEntity;
+import com.burgess.banana.system.util.R;
+
+/**
+ * 用户Token
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-23 15:22:07
+ */
+public interface SysUserTokenService extends IService<SysUserTokenEntity> {
+
+	/**
+	 * 生成token
+	 * @param userId  用户ID
+	 */
+	R createToken(long userId);
+
+	/**
+	 * 退出，修改token值
+	 * @param userId  用户ID
+	 */
+	void logout(long userId);
+
+}
