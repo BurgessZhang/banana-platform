31a499b4eb605d4d99cea78e5357a478f6b90199
tom.zhang <tom.zhang@all-dream.com>
2018-05-24 14:07:05 +0800

    update

---
+++ banana-platform-api/src/main/java/com/burgess/banana/api/util/BananaApiUtils.java
@@ -0,0 +1,12 @@
+package com.burgess.banana.api.util;
+
+/**
+ * @author tom.zhang
+ * @project banana-platform
+ * @package com.burgess.banana.api.util
+ * @file BananaApiUtils.java
+ * @time 2018-05-24 14:02
+ * @desc api工具类
+ */
+public class BananaApiUtils {
+}
---
+++ banana-platform-mongo/src/main/java/com/burgess/banana/mongo/util/BananaMongoUtils.java
@@ -0,0 +1,13 @@
+package com.burgess.banana.mongo.util;
+
+/**
+ * @author tom.zhang
+ * @project banana-platform
+ * @package com.burgess.banana.mongo.util
+ * @file BananaMongoUtils.java
+ * @time 2018-05-24 14:00
+ * @desc mongodb工具类
+ */
+public class BananaMongoUtils {
+
+}
